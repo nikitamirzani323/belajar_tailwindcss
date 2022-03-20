3054010828d1f7a392643f81434179cb9a8f46b3
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2022-03-21 06:08:06 +0700

    update animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ module.exports = {
     extend: {
       fontFamily: {
         inter: ['Inter'],
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        'spin-goyang': 'goyang 1s ease-in-out infinite',
+      },
+      keyframes: {
+        goyang: {
+          '0%, 100%': {
+            transform: 'rotate(-3deg)'
+          },
+          '50%' : {
+            transform: 'rotate(3deg)'
+          }
+        }
       }
     },
   },
